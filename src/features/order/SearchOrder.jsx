7c080ccf54c55ce532b699c06ba0c639567ea1d0
Ibraheem Shaheen <he.mo234@yahoo.com>
2025-08-20 06:55:49 +0300

    styling form elemenst

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -20,6 +20,7 @@ function SearchOrder() {
         placeholder="Search order#"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="placeholder:text-stone- w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 focus:ring-offset-2 sm:w-64 sm:focus:w-72"
       />
     </form>
   );
