61a9797b0e280613649b029219db42d9dca6aae2
Ibraheem Shaheen <he.mo234@yahoo.com>
2025-08-20 08:23:50 +0300

    configuring tailwind

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -11,7 +11,7 @@ function AppLayout() {
 
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
-      {true && <Loader />}
+      {isLoading && <Loader />}
 
       <Header />
 
