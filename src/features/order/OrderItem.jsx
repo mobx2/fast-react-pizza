f3394e606abcf292bc13e888e36fb07e32e92d28
Ibraheem Shaheen <he.mo234@yahoo.com>
2025-08-25 08:41:45 +0300

    updating priority

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -3,14 +3,19 @@ import { formatCurrency } from "../../utils/helpers";
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
+  console.log(isLoadingIngredients);
+
   return (
-    <li className="py-3">
-      <div className="tex-sm flex items-center justify-between gap-4">
+    <li className="space-y-1 py-3">
+      <div className="tex-[0.5rem] flex items-center justify-between gap-4">
         <p>
           <span className="font-bold">{quantity}&times;</span> {name}
         </p>
         <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
+      <p className="text-tone-500 text-sm capitalize italic">
+        {isLoadingIngredients ? "Loading..." : ingredients.join(",")}
+      </p>
     </li>
   );
 }
